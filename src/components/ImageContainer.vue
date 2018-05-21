fbf17c12ce89ee88b50ecd44f3bc210c958b1570
PS <PS@Apples-MacBook-Pro-3.local>
2018-05-21 17:45:32 +0530

    image uplaods

--- src/components/ImageContainer.vue
+++ src/components/ImageContainer.vue
@@ -1,72 +1,58 @@
 <template>
-<div class="container">
-
+    <div class="container">
       <h1 class="my-4 text-center text-lg-left">Thumbnail Gallery</h1>
-        <input type="file" id="files" name="files[]" multiple @change="handleFileSelect" />
-        <div class="row text-center text-lg-left" id="list"></div>
-      <div class="row text-center text-lg-left">
-
-        <div class="col-lg-3 col-md-4 col-xs-6" v-for="img in images">
-          <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" :src=img.icon alt="">
-          </a>
-        </div>
-     
+      <p>Select images, they will be stored in your browser and will be displayed even after reload. Selecting big images (&gt; 2MB) can crash your browser.</p>
+      <input type="file" id="files" @change="handleFileSelect">
+      <p id="state">No images stored in your browser.</p>
+      <div id="list">
       </div>
+      <!-- <a href="#" id="deleteImgs" @click="deleteImages">Delete Images</a> -->
+      <div class="row text-center text-lg-left">
+        <ul>
+          <li v-for="img in imagesObject">{{img}}</li>
 
+
+        </ul>
+        <ul>
+          <li v-for="img in imagesObject">
+            <img :src="img" alt="">
+          </li>
+        </ul>
+      </div>
     </div>    
 </template>
 <script>
 export default {
     data() {
         return {
-            images : [
-                {'icon' : 'http://placehold.it/400x300'},
-                {'icon' : 'http://placehold.it/400x300'},
-                {'icon' : 'http://placehold.it/400x300'},
-                {'icon' : 'http://placehold.it/400x300'},
-                 {'icon' : 'http://placehold.it/400x300'},
-                  {'icon' : 'http://placehold.it/400x300'}
-            ]
+            imagesObject : [ ],
+            imgData : ''
         }
     },
     methods : {
-   handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
 
-    // Loop through the FileList and render image files as thumbnails.
-    for (var i = 0, f; f = files[i]; i++) {
+     handleFileSelect(evt) {
+        var files = evt.target.files; // FileList object
+       
+        // Loop through the FileList and render image files as thumbnails.
+        for (var i = 0, f; f = files[i]; i++) {
 
-      // Only process image files.
-      if (!f.type.match('image.*')) {
-        continue;
-      }
-
-      var reader = new FileReader();
-
-      // Closure to capture the file information.
-      reader.onload = (function(theFile) {
-        return function(e) {
-            
-          // Render thumbnail.
-          console.log('result', e.target.result);
+          // Only process image files.
+          if (!f.type.match('image.*')) {
+            continue;
+          }
+          var reader = new FileReader();
+        
+          // Closure to capture the file information.
+          reader.onload = (e) => {this.imgData = e.target.result, this.imagesObject.push(this.imgData)};
           
-          var div = document.createElement('div');
-          div.classList = 'col-lg-3 col-md-4 col-xs-6';
-          div.innerHTML = ['<a class="d-block mb-4 h-100"><img class="thumb img-fluid img-thumbnail" src="', e.target.result,
-                            '" title="', escape(theFile.name), '"/></a>'].join('');
-          document.getElementById('list').insertBefore(div, null);
-        };
-      })(f);
-
-      // Read in the image file as a data URL.
-      reader.readAsDataURL(f);
+          //this.imagesObject.push(imgData)
+          console.log('this imagesObject' , this.imagesObject);
+          reader.readAsDataURL(f);
+        }
     }
-  }
-
-  //document.getElementById('files').addEventListener('change', handleFileSelect, false);
+    }
     
-}
 }
 </script>
 <style>
