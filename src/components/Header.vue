efaac5d12b2a44dbed83e1c21a6cec167ae4bb25
Javid <singularity.javid@gmail.com>
2018-05-22 16:45:27 +0530

    added LS and Date

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -42,7 +42,7 @@ export default {
   },
   methods : {
     uploadImage() {
-      console.log('image uplaoded');
+     //this.$parent.handleFileSelect();
     }
   }
 }
--- src/components/ImageContainer.vue
+++ src/components/ImageContainer.vue
@@ -2,7 +2,12 @@
     <div class="container">
       <h1 class="my-4 text-center text-lg-left">Thumbnail Gallery</h1>
       <p>Select images, they will be stored in your browser and will be displayed even after reload. Selecting big images (&gt; 2MB) can crash your browser.</p>
-      <input type="file" id="files" @change="handleFileSelect">
+      <!-- <input type="file" id="files" @change="handleFileSelect"> -->
+       <label class="btn btn-default" @change="handleFileSelect">
+            Add Image <input type="file" accept="image/*" hidden />
+        </label>
+
+
       <p id="state">No images stored in your browser.</p>
       <div id="list">
         
@@ -10,77 +15,97 @@
       <div class="row text-center text-lg-left">
         <div  class="col-lg-3 col-md-4 col-xs-6" v-for="(img, index) in imagesObject">
           <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail"  :src="img" alt="">
+            <img class="img-fluid img-thumbnail"  :src="img.imageData" alt="">
              <button class="btn btn-danger btn-small" @click="deleteThisimage(index, $event)">Delete</button>
+               <span>{{ img.dateUploaded }}</span>
            </a>
         </div>
+        
         <hr>
    </div>
       <div >
             <h2 class="alert alert-info">Generate a Carousal</h2>
-            
         </div>
         <div class="row">
            <lightbox v-for="(img, index) in imagesObject"
-             :thumbnail="img"
+              :thumbnail="img.imageData"
               :images="imagesObject"
             ></lightbox>
         </div>
         <hr>
      
       <button @click="deleteAllImages" class="btn btn-danger btn-block ">Delete All Images</button>
+    
       <hr>
     </div>    
 </template>
 <script>
+import moment from 'moment';
 
 export default {
-    data() {
-        return {
-            imagesObject : [ ],
-            imgData : ''
-            
-        }
-    },
-    components : {
-      
-    },
-    methods : {
-
-     handleFileSelect(evt) {
-        var files = evt.target.files; // FileList object
-       
-        // Loop through the FileList and render image files as thumbnails.
-        for (var i = 0, f; f = files[i]; i++) {
-
-          // Only process image files.
-          if (!f.type.match('image.*')) {
-            continue;
-          }
-          var reader = new FileReader();
+  data() {
+    return {
+      imagesObject: [
         
-          // Closure to capture the file information.
-          reader.onload = (e) => {
-                              console.log('Uplaoded At',   files[0].lastModified);
-                              this.imgData = e.target.result,
-                              this.imagesObject.push(this.imgData);
-                              localStorage.setItem("images", JSON.stringify(this.imagesObject));};
-          
-          //this.imagesObject.push(imgData)
-          //console.log('this imagesObject' , this.imagesObject);
-          reader.readAsDataURL(f);
+      ],
+      carousalImages : this.imagesObject,
+      imageData: "",
+      //dateField: ""
+    };
+  },
+  components: {},
+  methods: {
+
+    handleFileSelect(evt) {
+      var files = evt.target.files; // FileList object
+      // Loop through the FileList and render image files as thumbnails.
+      for (var i = 0, f; (f = files[i]); i++) {
+        // Only process image files.
+        if (!f.type.match("image.*")) {
+          continue;
         }
+        var reader = new FileReader();
+        // Closure to capture the file information.
+        reader.onload = e => {
+          console.log("ImageData", e.target.result);
+          console.log("Uplaoded At", new Date(e.timeStamp * 1000));
+          this.imagesObject.dateUploaded = new Date(e.timeStamp * 1000);
+          this.imagesObject.push({'imageData' : e.target.result, 'dateUploaded' : moment().format('LLLL') });
+          localStorage.setItem("images", JSON.stringify(this.imagesObject));
+          //localStorage.setItem("DU", JSON.stringify(moment().format('LLLL')));
+        
+        };
+
+        console.log(this.dateField);
+        //this.imagesObject.push(imgData)
+        //console.log('this imagesObject' , this.imagesObject);
+        reader.readAsDataURL(f);
+
+        // reader.onloadend = () => {
+        //   let d = new Date();
+        //   this.dateField =
+        //     d.getDate() +
+        //     "-" +
+        //     (d.getMonth() + 1) +
+        //     "-" +
+        //     d.getFullYear() +
+        //     " " +
+        //     d.getHours() +
+        //     ":" +
+        //     d.getMinutes();
+        //   //  localStorage.setItem("dateField", JSON.stringify(this.dateField));
+        // };
+      }
     },
-    loadFromLocalStorage(){
-      var images = JSON.parse(localStorage.getItem("images"))
-    console.log('images', images);
-      if(images && images.length > 0){
+    loadFromLocalStorage() {
+      var images = JSON.parse(localStorage.getItem("images"));
+      console.log("images", images);
+      if (images && images.length > 0) {
         this.imagesObject = images;
         //let listofImages = []
-       }
-      else {
+      } else {
         //let listofImages = [require('../assets/garden-rose1.jpeg'),require('../assets/ultra.jpg'), require('../assets/hiking.jpg') ];
-        // var dummyImage; 
+        // var dummyImage;
         // console.log('Local Storage is empty', this.imagesObject);
         // let dummyImage1 = require('../assets/garden-rose1.jpeg');
         // this.imagesObject.push(dummyImage1);
@@ -92,39 +117,39 @@ export default {
         // let dummyImage5 = require('../assets/greenpark.jpg')
         // let dummyImage6 = require('../assets/scenery.jpg')
         // let dummyImage7 = require('../assets/tiger.jpeg')
-         
-         
-       
-       // this.imagesObject = listofImages;
+        // this.imagesObject = listofImages;
       }
     },
     deleteThisimage(index, e) {
-      console.log('images are', this.imagesObject);
-      localStorage.removeItem('images');
-      this.imagesObject.splice(index, 1)
+      console.log("images are", this.imagesObject);
+      localStorage.removeItem("images");
+      this.imagesObject.splice(index, 1);
     },
     deleteAllImages() {
-      localStorage.removeItem('images');
+      localStorage.removeItem("images");
       this.imagesObject = [];
-    }
     },
-    mounted() {
-      console.log('I got mounted');
-      this.loadFromLocalStorage();
-    }
-    
-}
+     moment: function (date) {
+      return moment(date);
+    },
+  },
+
+  mounted() {
+    console.log("I got mounted");
+    this.loadFromLocalStorage();
+  }
+};
 </script>
 <style>
 .img-thumbnail {
-  border:1px solid red;
-  width:400px;
+  border: 1px solid red;
+  width: 400px;
   height: 300px;
 }
 .lightbox__thumbnail {
   border: 2px solid green;
-    display: block;
-    width: 50px !important;
-    height: 50px !important;
+  display: block;
+  width: 50px !important;
+  height: 50px !important;
 }
 </style>
