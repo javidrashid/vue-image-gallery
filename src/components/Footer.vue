b8bc7a1dcdcfe79115b5f470f60f9ef7cc66d854
Javid <singularity.javid@gmail.com>
2018-05-23 23:58:50 +0530

    node sass suport and pagination bugs fixed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="py-5 bg-dark">
       <div class="container">
-        <p class="m-0 text-center text-white">VueJS Powered Gallery With LocalStorage Support &copy</p>
+        <p class="m-0 text-center text-white">VueJS Powered Gallery With LocalStorage Support :)</p>
       </div>
       <!-- /.container -->
     </footer>
--- src/components/ImageContainer.vue
+++ src/components/ImageContainer.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
       <h1 class="my-4 text-center text-lg-left">Vue Image Gallery with LocalStorage Support</h1>
-      <p>Select images, they will be stored in your browser and will be displayed even after reload. Selecting big images (&gt; 2MB) can crash your browser.</p>
+      <p>Select images, they will be stored in your browser and will be displayed even after reload. Selecting big images (&gt; 10MB) can crash your browser.</p>
       <!-- <input type="file" id="files" @change="handleFileSelect"> -->
        <label class="btn btn-success" @change="handleFileSelect">
             Add Image <input type="file" accept="image/*" hidden />
@@ -11,7 +11,7 @@
           <a href="#" class="d-block mb-4 h-100">
             <img class="img-fluid img-thumbnail" :src="img.imageData" alt="">
                <p class="alert alert-info font-weight-light">Uploaded On <span class="green">{{ img.dateUploaded }}</span></p>
-               <button class="btn btn-block btn-sm btn-danger btn-small" @click="deleteThisimage(index, $event)">Delete</button>
+               <button class="btn btn-block btn-sm btn-danger btn-small" @click="deleteThisImage(index, $event)">Delete</button>
              </a>
         </div>
         <hr>
@@ -19,6 +19,7 @@
       <div class="carousal-area"  >
         <h2 class="alert alert-info" :hide="hideBanner">Click below to See Zoomed View</h2>
         <div class="row container">
+         
            <lightbox v-for="(img, index) in imagesObject"
               :thumbnail=img.imageData
               :images="[img.imageData]"
@@ -28,16 +29,22 @@
      <button @click="deleteAllImages" class="btn btn-danger btn-block ">Delete All Images [Clear Local Storage]</button>
     </div>
       <hr>
-     <paginate
-        name="languages"
-        :list="langs"
-        :per="2"
-      >
-        <li v-for="lang in paginated('languages')">
-          {{ lang }}
-        </li>
-      </paginate>
-      <paginate-links for="languages"></paginate-links>
+
+      <div class="withpagination">
+         <h2 class="alert alert-info" :hide="hideBanner">View Images Using Pagination</h2>
+          <paginate
+          name="images"
+          :list="bannerImage"
+          :per="1"
+          >
+            <li v-for="lang in paginated('images')">
+              <img :src="lang" alt="">
+            </li>
+        </paginate>
+         <paginate-links for="images" :limit="2" :show-step-links="true"></paginate-links>
+      </div>
+     
+      
 
     </div>    
 </template>
@@ -53,7 +60,7 @@ export default {
       hideBanner : true,
       bannerImage : [],
       langs: ['JavaScript', 'PHP', 'HTML', 'CSS', 'Ruby', 'Python', 'Erlang'],
-      paginate: ['languages']
+      paginate: ['images']
     };
     
   },
@@ -71,7 +78,8 @@ export default {
         reader.onload = e => {
           this.imagesObject.dateUploaded = new Date(e.timeStamp * 1000);
           this.imagesObject.push({'imageData' : e.target.result, 'dateUploaded' : moment().format('LLLL') });
-          this.galleryImages.push(e.target.result)
+          this.galleryImages.push(e.target.result);
+          this.bannerImage.push(e.target.result);
           localStorage.setItem("images", JSON.stringify(this.imagesObject));
            this.$toastr('success', 'Image Added Successfully to Gallery', 'Image Added!!!');
            console.log('after upload', this.imagesObject);
@@ -89,16 +97,19 @@ export default {
         //Incase we want to load Default Images
       }
     },
-    deleteThisimage(index, e) {
-      localStorage.removeItem("images");
+    deleteThisImage(index, e) {
+      console.log('calling delete on singal file');
       this.imagesObject.splice(index, 1);
-      this.$toastr('error', 'Image Deleted Successfully From LocalStorage', 'Image Deleted!!!');
+      this.bannerImage.splice(index, 1);
+      localStorage.removeItem(this.imagesObject.splice(index, 1))
+      this.$toastr('error', 'Image Deleted Successfully From View', 'Image Deleted!!!');
     },
     deleteAllImages() {
       localStorage.removeItem("images");
       this.imagesObject = [];
+      this.bannerImage = [];
        this.$toastr('error', 'All Image have been deleted Successfully From LocalStorage', 'Cleared LocalStorage!!!');
-      this.hideBanner = true;
+      //this.hideBanner = true;
     },
      moment: function (date) {
       return moment(date);
@@ -117,94 +128,85 @@ export default {
   }
 };
 </script>
-<style>
+<style lang="scss" >
+
 .img-thumbnail {
-  
-  width: 400px;
-  height: 300px;
+    width: 400px;
+    height: 300px;
 }
+
 .lightbox__thumbnail {
-  display: block;
-  width: 30px !important;
-  height: 30px !important;
+    display: block;
+    width: 30px !important;
+    height: 30px !important;
 }
+
 .font-weight-light {
-  font-size:12px;
- 
-  
+    font-size: 12px;
 }
+
 a {
-  /* border: 2px solid rgba(0, 0, 0, 0.678); */
-   cursor: auto;
-   text-decoration: none !important;
+    /* border: 2px solid rgba(0, 0, 0, 0.678); */
+    cursor: auto;
+    text-decoration: none !important;
 }
+
 .alert-info span {
-  color : red;
-}
-
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  font-size: 20px;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1,
-h2 {
-  font-weight: normal;
+    color: red;
 }
 
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
 
 li {
-  display: inline-block;
-  margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
 
 .paginate-list {
-  width: 159px;
-  margin: 0 auto;
-  text-align: left;
-  li {
-    display: block;
-    &:before {
-      content: '⚬ ';
-      font-weight: bold;
-      color: slategray;
+    width: 159px;
+    margin: 0 auto;
+    text-align: left;
+    li {
+        display: block;
+        &:before {
+            content: '⚬ ';
+            font-weight: bold;
+            color: slategray;
+        }
     }
-  }
 }
 
 .paginate-links.items {
-  user-select: none;
-  a {
-    cursor: pointer;
-  }
-  li.active a {
+    user-select: none;
+}
+
+li.active a {
     font-weight: bold;
-  }
-  li.next:before {
-    content: ' | ';
-    margin-right: 13px;
-    color: #ddd;
-  }
-  li.disabled a {
-    color: #ccc;
-    cursor: no-drop;
-  }
 }
 
 a {
-  color: #42b983;
+    cursor: pointer;
 }
 
+li.next:before {
+    content: ' | ';
+    margin-right: 13px;
+    color: #ddd;
+}
 
+li.disabled a {
+    color: #ccc;
+    cursor: no-drop;
+}
 
+a {
+    color: #42b983;
+}
+
+.withpagination {
+    text-align: center;
+}
 </style>
