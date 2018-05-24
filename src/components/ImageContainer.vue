4d71ff040bdedd1e2656749f6adf9f9d7457099c
Javid <singularity.javid@gmail.com>
2018-05-24 12:43:05 +0530

    added magnified component

--- src/components/ImageContainer.vue
+++ src/components/ImageContainer.vue
@@ -15,17 +15,7 @@
         </div>
         <hr>
    </div>
-      <div class="carousal-area" v-show="imagesObject.length > 0">
-        <h4 class="alert alert-info" >Click below to See Zoomed View</h4>
-        <div class="row container">
-           <lightbox v-for="(img, index) in imagesObject"
-              :thumbnail=img.imageData
-              :images="[img.imageData]"
-            ></lightbox>
-        </div>
-        <hr>
-     <button @click="deleteAllImages" class="btn btn-danger btn-block ">Delete All Images [Clear Local Storage]</button>
-    </div>
+      <magnified-view :imagesObject="imagesObject" @deleteAllImages="deleteAllImages"></magnified-view>
       <hr>
       <paginate :bannerImage="bannerImage" :imagesObject="imagesObject"></paginate>
     </div>    
@@ -33,6 +23,7 @@
 <script>
 import moment from 'moment';
 import paginate from './Pagination'
+import magnifiedView from './MagnifiedView'
 export default {
   data() {
     return {
@@ -44,7 +35,8 @@ export default {
     };
   },
   components : {
-    paginate
+    paginate,
+    'magnified-view' : magnifiedView
   },
   methods: {
     handleFileSelect(evt) {
---
+++ src/components/MagnifiedView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="carousal-area" v-show="imagesObject.length > 0">
+        <h4 class="alert alert-info" >Click below to See Zoomed View</h4>
+        <div class="row container">
+           <lightbox v-for="(img, index) in imagesObject"
+              :thumbnail=img.imageData
+              :images="[img.imageData]"
+            ></lightbox>
+        </div>
+        <hr>
+     <button @click="deleteAllImagesinParent" class="btn btn-danger btn-block ">Delete All Images [Clear Local Storage]</button>
+    </div>
+</template>
+<script>
+export default {
+     props : ['imagesObject'],
+    data() {
+        return {
+        }
+    },
+   methods: {
+        deleteAllImagesinParent() {
+            this.$emit('deleteAllImages')
+        }
+    }
+}
+</script>
+<style>
+
+</style>
+
